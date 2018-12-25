36c062b8cc9cfe16185408e8b9bc785186816873
shanmuk sai <shanmuksai1@gmail.com>
2018-12-25 22:17:09 +0530

    included angular

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
 // Initialize the app
 let app = express();
-
+let today = new Date();
 mongoURl='mongodb://slnop:shanmuks1@ds243254.mlab.com:43254/slnop';
 
 app.use(bodyParser.json());
@@ -25,13 +25,20 @@ app.use(bodyParser.json());
  }
 // Setup server port
 var port = process.env.PORT || 8081;
+app.use(express.static(__dirname+'/angular_app/billing-ui/'));
 // Send message for default URL
+app.get('/data', async (req, res) =>{ 
+    let data = await sai('data');
+     result = await data.find({}).toArray();
+     console.log(today);
+    res.send({data :result});
+});
 app.get('/', async (req, res) =>{ 
     let data = await sai('data');
      result = await data.find({}).toArray();
-    res.send({data :result});
+     console.log(__dirname);
+     res.status(200).sendFile(__dirname+'/angular_app/billing-ui/index.html'); 
 });
-
 // Launch app to listen to specified port
 app.listen(port, function () {
     console.log("Running RestHub on port " + port);
