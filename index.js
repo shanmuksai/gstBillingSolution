70702a57c1d3d34700d7004b53722e900d29eaa9
shanmuk sai <shanmuksai1@gmail.com>
2018-12-27 01:32:12 +0530

    added functionality'

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ let express = require('express');
 // Import Body parser
 let bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
+const cors = require('cors');
 // Initialize the app
 let app = express();
+//for cors 
+app.use(cors());
+app.options('*', cors());
 let today = new Date();
 mongoURl='mongodb://slnop:shanmuks1@ds243254.mlab.com:43254/slnop';
 
@@ -33,12 +37,21 @@ app.get('/data', async (req, res) =>{
      console.log(today);
     res.send({data :result});
 });
+
 app.get('/', async (req, res) =>{ 
     let data = await sai('data');
      result = await data.find({}).toArray();
      console.log(__dirname);
      res.status(200).sendFile(__dirname+'/angular_app/billing-ui/index.html'); 
 });
+
+app.post('/save', async (req, res) =>{ 
+    let data = await sai('data');
+    req.body._id = await data.count()+1;
+     result = await data.insertOne(req.body);
+     res.status(200).send(req.body);
+});
+
 // Launch app to listen to specified port
 app.listen(port, function () {
     console.log("Running RestHub on port " + port);
